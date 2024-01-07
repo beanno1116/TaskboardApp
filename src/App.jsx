03fcb848060b180face06351f171fed8af87a3f5
Ben <beanno1116@gmail.com>
2024-01-07 07:56:15 -0500

    Refactored login view

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import useLocalStorage from './hooks/useLocalStorage';
 import { devFetchContacts } from './data/fakeApi';
 
 function App() {
-  const [isAuth,setAuth] = useLocalStorage('is-auth',{status: false});
+  const [isAuth,setAuth] = useLocalStorage('is-auth',false);
   const users = devFetchContacts();
   // setAuth(false);
 
@@ -22,15 +22,45 @@ function App() {
   }
 
   const handleLogin = (e,formData) => {
-    const {username,password} = formData;
-    const results = users.filter(u => u.email === username);
-    if (results.length > 0) {
-      alert(`Welcome ${results[0].first_name} ${results[0].last_name}`)
-    }
-    let tmp = formData;
+
+    async function fetchData(_url,_options) {
+      try {
+          const res = await fetch(_url,_options);
+          const json = await res.text();
+          
+          // const json = await res.json();
+          return json;
+      } catch (e) {
+          console.log(e);
+          return e;            
+      }        
+  }
+
+  fetch("http://172.105.4.73/api/taskboard/api.php",{method:"GET"})
+  .then((response) => response.text())
+  .then(data => {
+    //handle data
     debugger;
-    console.log(formData);
-    setAuth(true);
+    console.log(data);
+  })
+  .catch(error => {
+    //handle error
+  });
+
+  // fetchData("http://172.105.4.73/api/taskboard/api.php", {method:"GET",mode: 'no-cors'}).then(response => {
+  //   debugger;
+  //   console.log(response);
+  // })
+
+    // const {username,password} = formData;
+    // const results = users.filter(u => u.email === username);
+    // if (results.length > 0) {
+    //   alert(`Welcome ${results[0].first_name} ${results[0].last_name}`)
+    // }
+    // let tmp = formData;
+    // debugger;
+    // console.log(formData);
+    // setAuth(true);
   }
 
   
--- src/Views/Login/LoginView.jsx
+++ src/Views/Login/LoginView.jsx
@@ -3,6 +3,8 @@ import image from '../../assets/images/loginImage.png';
 import styles from './loginView.module.css';
 
 import useWEForm from '../../hooks/useWEForm';
+import Panel from './components/Panel';
+import LoginForm from './forms/LoginForm';
 
 const LoginView = ({ onSubmit }) => {
   const {formData,registerFormInput,handleSubmit} = useWEForm({
@@ -16,44 +18,18 @@ const LoginView = ({ onSubmit }) => {
   }
 
   return (
-    <div className={`${styles.view} ${styles.login}`}>
-       
+    <div className={`${styles.view} ${styles.login}`}>       
       <div className={styles.login_form_wrapper}>
 
-        <div className={`${styles.panel} ${styles.image}`}>
+        <Panel className={styles.image_panel}>
           <img src={image} />
-        </div>
+        </Panel>
 
-        <div className={`${styles.panel} ${styles.form}`}>
-
-          <form className={styles.login_form}>
-
-            <div className={styles.input_row}>
-              <h1 className={styles.form_heading}>Login</h1>
-            </div>
-
-            <div className={styles.input_row}>
-                <input className={styles.login_form_input}  {...registerFormInput("username")} placeholder={"Username"} />                  
-            </div>
-            <div className={styles.input_row}>
-                <input className={styles.login_form_input} type={"password"} {...registerFormInput("password")} placeholder={"Password"} />                  
-            </div>
-
-            <div className={styles.input_row}>
-              <a className={styles.reset_link}>Forgot password?</a>
-            </div>
-
-            <div className={styles.input_row}>
-              <button type='submit' className={styles.add_btn} onClick={e => handleSubmit(e,onSubmit)}>Login</button>
-              <button type='button' className={`${styles.add_btn} ${styles.no_bg}`}>Reset</button>
-            </div>
-
-          </form>
-
-        </div>
-
+        <Panel className={styles.form_panel}>
+          <LoginForm onSubmit={onSubmit} />
+        </Panel>
+        
       </div>
-
     </div>
   );
 }
